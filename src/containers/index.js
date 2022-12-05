defe19b79f77bc6534300c3dbdcc055c29779fbd
A. Elif Büyük <76538049+djvra@users.noreply.github.com>
2022-12-05 17:18:25 +0300

    Delete src directory

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FavoritesContainer } from './influencerContainer/FavoritesContainer';
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
